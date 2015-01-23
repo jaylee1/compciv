c7923114e1b287df164b6cd31a376f1ad55a4af3
Jay Lee <jaylee1@corn02.stanford.edu>
2015-01-22 19:51:13 -0800

    briefings directory

---
+++ homework/wh-briefings-word-scrape/helper.sh
@@ -0,0 +1,8 @@
+cat * |~/bin_compciv/pup 'p text{}' | grep -oE '[[:alpha:]]{7,$
+
+cat * |~/bin_compciv/pup '.legacy-para text{}' | grep -oE '[[:$
+
+cat words.txt | grep -oE '[[:alpha:]]{7,}+'| sort | uniq -c | $
+
+rm words.txt
+
